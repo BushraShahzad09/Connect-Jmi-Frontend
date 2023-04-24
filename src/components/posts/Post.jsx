9d5e6d8fc0a3aa51e0a4d5781a0ea05b496eef11
Bushra Shahzad <bushra05shahzad09>
2023-04-24 19:18:08 +0530

    formatting of comments

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
 import { useState } from "react";
 import moment from 'moment';
+import { Box, Stack, Avatar } from "@mui/material";
 
 const Post = ({ post }) => {
 
@@ -14,11 +15,27 @@ const Post = ({ post }) => {
     setCommentOpen(!commentOpen);
   }
 
+  const getName = (value) => {
+
+    return `${value.split(" ")[0][0]}`;
+  }
+
   return (
     <div className="post">
       <div className="userInfo">
         <div className="details">
+          <Box >
+            <Stack spacing={2}>
+              <Avatar className="avatar"
+                sx={{ bgcolor: "lightblue", color: "black" }}
+                children={getName(`${post.name}`)}
+              />
+            </Stack>
+
+          </Box>
           <span className="name">{post.name}</span>
+        </div>
+        <div className="timestamp">
           <span className="date">{moment(post.createdat).fromNow()}</span>
         </div>
         <div className="content">
@@ -26,11 +43,14 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <button id="comments" className={commentOpen ? "item commentActive" : "item"} onClick={handleComment}>
-            <TextsmsOutlinedIcon style={{marginRight: 5}}/>
+            <TextsmsOutlinedIcon style={{ marginRight: 5 }} />
             Comments
           </button>
         </div>
+
         {commentOpen && <Comments postId={post.id} />}
+
+
       </div>
     </div>
   );
