834b67f41b9caf0a4b48bca757b89a5acee5ab12
rrmsha <ramshaanwar11@gmail.com>
2023-04-26 20:34:08 +0530

    profile section added and comments validation

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useContext, useState } from "react";
+import logo from "../../images/icon.jpg"
+import { AuthContext } from "../../context/authContext";
+import "./Profile.css"
+
+const Profile = () => {
+    const { currentUser } = useContext(AuthContext);
+    return(
+        <div className="profile-container">
+            <div className="profile-background">
+            </div>
+            <div className="white-container">
+                <img className="profile-icon" alt="logo" src={logo} width="100px"/>
+            </div>
+            
+            <h1 className="profile-name">{currentUser.name}</h1>
+            <p className="bio">Bio</p>
+            <div className="profile-post">Posts Number</div>
+            <div className="profile-comments">Comments Number</div>
+            <a className="profile-edit">Edit Profile</a>
+        </div>
+    )
+}
+
+export default Profile;
